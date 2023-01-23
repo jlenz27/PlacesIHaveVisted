3454c07b1a822d334075a474719b730c1340aea3
John Lenz <120522526+jlenz27@users.noreply.github.com>
2023-01-23 12:37:40 -0800

    Updated js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
-// function placesBeen(){
-//     this.locations = {};
-//     this.timeOfYear = {};
-//     this.landmark = {};
-//     this.notes = {};
-// }
+function PlaceEntry() {
+    this.locations = {};
+    this.currentId = 0;
+};
 
-// placesBeen.prototype.addLocation = function(location){
-//     location.id = this.assignId();
-//     this.location[location.id] = location;
+PlaceEntry.prototype.addLocation = function (location) {
+    location.id = this.assignId();
+    this.locations[location.id] = location;
 
-// }
+};
+
+PlaceEntry.prototype.assignId = function () {
+    this.currentId += 1;
+    return this.currentId;
+};
+
+PlaceEntry.prototype.findLocation = function (id) {
+    if (this.location[id] !== undefined) {
+        return this.location[id];
+    }
+    return false;
+};
+
+PlaceEntry.prototype.deleteLocation = function (id) {
+    if (this.location[id] === undefined) {
+      return false;
+    }
+    delete this.location[id];
+    return true;
+  };
+
 
 function PlacesBeen(locations, timeOfYear, landmark, notes) {
-  this.locations = locations;
-  this.timeOfYear = timeOfYear;
-  this.landmark = landmark;
-  this.notes = notes;
-}
+    this.locations = locations;
+    this.timeOfYear = timeOfYear;
+    this.landmark = landmark;
+    this.notes = notes;
+};
